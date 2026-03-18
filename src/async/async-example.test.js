10e77425b05d30f905e7ac75be079f80d58ca7af
David Ruiz <davidruizdeveloper@gmail.com>
2026-03-18 17:52:27 -0500

    refactor test for generateToken to handle async completion

--- src/async/async-example.test.js
+++ src/async/async-example.test.js
@@ -2,11 +2,17 @@ import { describe, it, expect } from 'vitest';
 import { generateToken, generateTokenPromise } from './async-example';
 
 describe('generateToken', ()=>{
-  it('should generate a token value', ()=>{
+  it('should generate a token value', (done)=>{
     const userEmail = 'example@example.com'
 
     generateToken(userEmail, (err, token)=>{
-      expect(token).toBeDefined();
+      try {
+        expect(token).toBeDefined();
+        // expect(token).toBe(2);
+        done()
+      } catch (error) {
+        done(error);
+      }
     })
   });
 });
