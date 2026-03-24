b2081cad0a66d8a2ab26a1829f5424ebae4bdb3c
David Ruiz <davidruizdeveloper@gmail.com>
2026-03-23 20:00:08 -0500

    feat: add test for writeData function to verify file writing

---
+++ src/util/io.test.js
@@ -0,0 +1,10 @@
+import { it, expect, describe } from 'vitest';
+import writeData from './io';
+
+describe('writeData()', ()=>{
+  it('should execute the writeFile method', ()=>{
+    const testData = 'Test';
+    const testFilename = 'test.txt'
+    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  })
+});
