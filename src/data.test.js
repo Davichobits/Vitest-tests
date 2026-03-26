303cb9b74a0ee784c638937aa2069b192fe9eae0
David Ruiz <davidruizdeveloper@gmail.com>
2026-03-25 20:02:59 -0500

    notes on spies

--- src/data.test.js
+++ src/data.test.js
@@ -4,9 +4,14 @@ import { generateReportData } from './data';
 describe('generateReportData()', ()=>{
 
   it('should call the callback function', ()=>{
+
     const logFn = vi.fn();
+
     generateReportData(logFn);
+
     expect(logFn).toBeCalled();
+    // expect(logFn).toBeCalledTimes(1);
+    // expect(logFn).toBeCalledWith(); // arguments
   })
 
 })
