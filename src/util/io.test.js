9691c781efaf756b3fca532008400d4569a89bce
David Ruiz <davidruizdeveloper@gmail.com>
2026-03-26 18:47:33 -0500

    refactor: enhance path mock implementation in writeData test

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -3,14 +3,26 @@ import { promises } from 'fs'
 import writeData from './io';
 
 vi.mock('fs');
+vi.mock('path', ()=>{
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length -1];
+      }
+    }
+  }
+})
 
 describe('writeData()', ()=>{
   it('should execute the writeFile method', ()=>{
+
     const testData = 'Test';
     const testFilename = 'test.txt'
+
     writeData(testData, testFilename)
     // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+    //expect(promises.writeFile).toBeCalled();
 
-    expect(promises.writeFile).toBeCalled();
+    expect(promises.writeFile).toBeCalledWith(testFilename, testData)
   })
 });
