484fe68bc776f2ca401598cd1d6412609bf121bd
David Ruiz <davidruizdeveloper@gmail.com>
2026-04-05 06:24:31 -0500

    test: update DOM test setup to include HTML structure and necessary imports

--- src/util/dom.test.js
+++ src/util/dom.test.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+
 import { showError } from './dom';
-import { it } from 'vitest';
+import { it, vi } from 'vitest';
+
+import {Window} from 'happy-dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window()
+const document = window.document;
+
+document.body.innerHTML = htmlDocumentContent;
+vi.stubGlobal('document', document);
 
 it('test', ()=>{
   showError('test');
